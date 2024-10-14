d744ff4caaf1cd43cd710f60a4d2a4b764c172cc
Parvinder Singh <ps8607474@gmail.com>
2024-10-14 17:17:41 +0530

    change for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Logo } from "@/features/editor/components/logo";
+
 export default function Home() {
   return (
     <>
-   <div className="">
-    Hello World
-   </div>
-    <Button size={"lg"} ><Link href="/editor">open Editor</Link></Button>
+      <div className="flex justify-center items-center h-screen bg-gradient-to-br from-purple-500 via-pink-500 to-red-500">
+        <div className="bg-white bg-opacity-70 p-8 rounded-lg shadow-lg text-center backdrop-filter backdrop-blur-md">
+          <div className="mb-8 w-2xl flex justify-center animate-pulse">
+            <Logo />
+          </div>
+          <h1 className="text-4xl font-extrabold text-gray-800 mb-4 shadow-lg">
+            Welcome to Editor
+          </h1>
+          <p className="text-lg text-gray-700 mb-8">
+            Create stunning designs with ease.
+          </p>
+          <Link href="/editor">
+            <Button
+              size={"lg"}
+              className="bg-blue-600 text-white hover:bg-blue-700 transition-transform transform hover:scale-105 shadow-lg"
+            >
+              Start Creating
+            </Button>
+          </Link>
+        </div>
+      </div>
     </>
-
   );
 }
